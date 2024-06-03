d973764ca8f307ec963ded29ee9a3d7def28ffb3
NunezIvan <142680907+NunezIvan@users.noreply.github.com>
2024-06-02 21:48:56 -0500

    Delete src directory

--- src/models/asociacion_vecinal_models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-class asociacion_vecinal(BaseModel):
-    id_asociacion:str
-    nombre:str
-    distrito:str
-    provincia:str
-    est_plus:bool
-    membresia:int
-    
