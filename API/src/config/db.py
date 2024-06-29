d2f9513164770637d6298004aa7bbb3d54273598
NunezIvan <nunezivan124@gmail.com>
2024-06-29 13:38:36 -0500

    Modificando el config

--- API/src/config/db.py
+++ API/src/config/db.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine, MetaData
+from dotenv import load_dotenv
+import os
 
-engine = create_engine("mysql+pymysql://root:KazumaHumilde123@localhost:3306/DB_BarrioSeguro",pool_pre_ping=True)
+# Cargar variables de entorno desde un archivo .env
+load_dotenv()
+
+# Obtener las variables de entorno
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Crear la conexión a la base de datos
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 meta_data = MetaData()
-
-
